3398ff8e7e0e99dd09557cf76c14d57861fa4e0d
LuisEnrique25 <luisrg2225@gmail.com>
2024-03-08 13:01:52 -0600

    login y seteo del estado con token creado

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Playlists from './pages/Playlists'
 import TrackDetail from './pages/TrackDetail'
 import ArtistsDetail from './pages/ArtistsDetail'
 import PlaylistPublic from './pages/PlaylistPublic'
+import PrivateRoutes from './components/Auth/PrivateRoutes'
+import Page404 from './pages/Page404'
 
 
 function App() {
@@ -16,17 +18,25 @@ function App() {
     <main >
         {/** Aqui va la configuracion de las rutas */}
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          {/**Rutas Publicas */}
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>} />
-          <Route path='/playlists' element={<Playlists/>} />
-          <Route path='/playlists/:id' element={<PlaylistDetail/>} />
-          <Route path='/tracks/:id' element={<TrackDetail/>} />
-          <Route path='/artists/:id' element={<ArtistsDetail/>} />
-
-
           <Route path='/playlists/public/:id' element={<PlaylistPublic/>}/>
+          
+          {/**Rutas Privadas */}
+           <Route element= {<PrivateRoutes/>} >
 
+            <Route path='/' element={<Home/>}/>
+            <Route path='/playlists' element={<Playlists/>} />
+            <Route path='/playlists/:id' element={<PlaylistDetail/>} />
+            <Route path='/tracks/:id' element={<TrackDetail/>} />
+            <Route path='/artists/:id' element={<ArtistsDetail/>} />
+        
+          </Route>
+
+        <Route path='*' element={<Page404/>}/>
+
+          
         </Routes>
       
     </main>
--- src/store/slices/user.slice.js
+++ src/store/slices/user.slice.js
@@ -9,16 +9,27 @@ const userSlice = createSlice({
         token: ""
     },
     reducers:{
-        
+        setLoginData: (state, action) => {
+            const loginData = action.payload; // -> {name:"", email:"", token:""}
+            //forma 1 para cambiar el estado
+            //state.email = loginData.email;
+            //state.name = loginData.name;
+            //state.token = loginData.token;
+            
+            //forma 2 para sobre escribir los valore del estado 
+            return {...state, ...loginData }
+        }
     },
 });
 
+const { setLoginData } = userSlice.actions
+
 export default userSlice.reducer;
 
-export const login = (data) =>  (dispath) => {
+export const login = (data) =>  (dispatch) => {
         //configuracion de axios en utils
         axiosMusic.post(`/api/auth/login`, data)  
-        .then(({ data }) => console.log(data))
+        .then(({ data }) => dispatch(setLoginData(data)))
         .catch((err) => console.log(err))
 
 }
